a4874d38b11b4ebf6fcde2e967eeeca0b911c6c5
bhavya2501 <bhavya.chopra@relevancelab.com>
2020-08-07 01:57:25 +0530

    changes in .gitignore

---
+++ client/build/precache-manifest.7f8f59d9767951a9d0d452a2b012f058.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a127176741e73dca5694dadafffaf02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "833102ae6cac5ae274f6",
+    "url": "/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "1be50a00931e1bc3b4d5",
+    "url": "/static/css/main.b14ac561.chunk.css"
+  },
+  {
+    "revision": "833102ae6cac5ae274f6",
+    "url": "/static/js/2.4c5bfd93.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.4c5bfd93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be50a00931e1bc3b4d5",
+    "url": "/static/js/main.34a34084.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
